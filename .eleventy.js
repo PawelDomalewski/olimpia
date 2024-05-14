2be8596eeafe5caf122a92be7f6c85e346f7b8bf
PawelDomalewski <pawel.domalewski5@wp.pl>
2024-05-14 19:15:00 +0200

    11ty initial

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy('./src/css/stylesheets.css');
+    eleventyConfig.addPassthroughCopy('./upload');
+
+    return {
+        dir: {
+            input: "src",
+            output: "_site"
+        }
+    }
+}
