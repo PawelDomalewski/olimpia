2f16aac568f6c03ade92c2dbd6be839aa469a6c8
PawelDomalewski <pawel.domalewski5@wp.pl>
2024-06-01 20:50:13 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,9 @@
 module.exports = function(eleventyConfig) {
 
-    eleventyConfig.addPassthroughCopy('./src/css/stylesheets.css');
-    eleventyConfig.addPassthroughCopy('./src/upload');
-    eleventyConfig.addPassthroughCopy("./src/admin");
-    eleventyConfig.addPassthroughCopy('./src/blog');
+    eleventyConfig.addPassthroughCopy("/src/css/stylesheets.css");
+    eleventyConfig.addPassthroughCopy("/src/upload");
+    eleventyConfig.addPassthroughCopy("/src/admin");
+    eleventyConfig.addPassthroughCopy("/src/blog");
     eleventyConfig.addWatchTarget("src/**/*.scss");
     eleventyConfig.addWatchTarget("src/**/*.css");
     eleventyConfig.addWatchTarget("src/**/*.js");
