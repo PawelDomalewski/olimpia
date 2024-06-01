a31a31304410dd5d49484b959ccee39ebef4c62a
PawelDomalewski <pawel.domalewski5@wp.pl>
2024-06-01 18:42:51 +0200

    add

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/css/stylesheets.css');
     eleventyConfig.addPassthroughCopy('./src/upload');
     eleventyConfig.addPassthroughCopy('./src/admin');
+    eleventyConfig.addPassthroughCopy('./src/blog');
     eleventyConfig.addWatchTarget("src/**/*.scss");
     eleventyConfig.addWatchTarget("src/**/*.css");
     eleventyConfig.addWatchTarget("src/**/*.js");
