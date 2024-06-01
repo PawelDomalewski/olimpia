bada2656c5efbd4918a863f8f54fec01c940ba86
PawelDomalewski <pawel.domalewski5@wp.pl>
2024-06-02 01:29:26 +0200

    Revert "Update .eleventy.js"
    This reverts commit aa84c7de7a4d482f011607f4f01a894beee580ce.

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("./src/css/stylesheets.css");
-    eleventyConfig.addPassthroughCopy("./src/upload/**/");
+    eleventyConfig.addPassthroughCopy("./src/upload");
     eleventyConfig.addPassthroughCopy("./src/admin");
     eleventyConfig.addPassthroughCopy("./src/blog");
     eleventyConfig.addWatchTarget("src/**/*.scss");
